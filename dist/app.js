aa23c54392febf8462fffc9fa45e78e4bec83da1
Maciej Chałapuk <maciej@chalapuk.pl>
2016-07-31 02:59:58 +0200

    + observing body element

--- dist/app.js
+++ dist/app.js
@@ -30108,9 +30108,15 @@ function observe($wnd) {
   }));
 
   observer.observeBrowserEvents($wnd, events);
+  observer.observeBrowserEvents($doc, events);
   observer.observePropertyChanges('window', $wnd, windowProperties, true);
   observer.observePropertyChanges('document', $doc, documentProperties.concat(offsetProperties), true);
   observer.observePropertyChanges('html', $html, offsetProperties, true);
+
+  $wnd.addEventListener('load', function() {
+    observer.observeBrowserEvents($doc.body, events);
+    observer.observePropertyChanges('body', $doc.body, offsetProperties, true);
+  });
 }
 
 observe(window);
